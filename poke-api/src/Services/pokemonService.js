1000f70a2be399780debeae10f61993b7dcbddf1
Eduardo <edalvarez2015@gmail.com>
2025-06-02 13:34:45 -0500

    npm install React-Router + OpenAI

--- poke-api/src/Services/pokemonService.js
+++ poke-api/src/Services/pokemonService.js
@@ -1,34 +1,33 @@
 // src/services/pokemonService.js
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'https://pokeapi.co/api/v2';
-
+const API_URL = "https://pokeapi.co/api/v2";
 // Fetch a list of pokemon with pagination
 export const getPokemonList = async (limit = 20, offset = 0) => {
   try {
-    const response = await axios.get(`${API_URL}/pokemon`, {
-      params: { limit, offset }
+    const response = await axios.get(`${API_URL}/pokemon/${inputPokemon}`, {
+      params: { limit, offset },
     });
-    
+
     // Extract the basic info and get the IDs from the URLs
-    const results = response.data.results.map(pokemon => {
-      const urlParts = pokemon.url.split('/');
+    const results = response.data.results.map((pokemon) => {
+      const urlParts = pokemon.url.split("/");
       const id = parseInt(urlParts[urlParts.length - 2]);
       return {
         id,
         name: pokemon.name,
-        url: pokemon.url
+        url: pokemon.url,
       };
     });
-    
+
     return {
       results,
       count: response.data.count,
       next: response.data.next,
-      previous: response.data.previous
+      previous: response.data.previous,
     };
   } catch (error) {
-    console.error('Error fetching Pokemon list:', error);
+    console.error("Error fetching Pokemon list:", error);
     throw error;
   }
 };
@@ -37,26 +36,26 @@ export const getPokemonList = async (limit = 20, offset = 0) => {
 export const getPokemonDetails = async (idOrName) => {
   try {
     const response = await axios.get(`${API_URL}/pokemon/${idOrName}`);
-    
+
     // Get the species details which include descriptions
     const speciesResponse = await axios.get(response.data.species.url);
-    
+
     return {
       id: response.data.id,
       name: response.data.name,
       height: response.data.height,
       weight: response.data.weight,
-      types: response.data.types.map(type => type.type.name),
-      abilities: response.data.abilities.map(ability => ability.ability.name),
-      stats: response.data.stats.map(stat => ({
+      types: response.data.types.map((type) => type.type.name),
+      abilities: response.data.abilities.map((ability) => ability.ability.name),
+      stats: response.data.stats.map((stat) => ({
         name: stat.stat.name,
-        value: stat.base_stat
+        value: stat.base_stat,
       })),
       sprites: response.data.sprites,
-      species: speciesResponse.data
+      species: speciesResponse.data,
     };
   } catch (error) {
-    console.error('Error fetching Pokemon details:', error);
+    console.error("Error fetching Pokemon details:", error);
     throw error;
   }
 };
